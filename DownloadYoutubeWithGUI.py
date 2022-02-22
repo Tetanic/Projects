52e4e7a6a312ed7de2ecbf57e7b2484028b9bdff
Clifford <cliffordgrow@gmail.com>
2022-02-21 20:19:25 -0500

    Works Somewhat

--- DownloadYoutubeWithGUI.py
+++ DownloadYoutubeWithGUI.py
@@ -4,6 +4,23 @@ from tkinter import messagebox
 from webbrowser import get
 root = Tk()
 
+def get_ytube(url_string):
+    from pytube import YouTube
+    link = url_string
+    yt = YouTube(link)
+    #Get Title
+    print(yt.title)
+    #Get Thumbnail Image
+    print(yt.thumbnail_url)
+    #select stream resolution
+    yt = yt.streams.get_highest_resolution()
+    #or yt = yt.streams.first()
+    # for video in yt.streams:
+    #     print(video)
+
+    yt.download()
+
+
 #Title, Icon, Size 
 
 root.title("Youtube Downloader")
@@ -14,36 +31,20 @@ root.geometry("700x350")
 
 my_label = Label(root, text = "Hello, Welcome to Clifford's Youtube Downloader ").pack()
 my_label2 = Label(root, text= "Please Paste the URL from Youtube.com").pack()
-gui_link = Entry(root, width = 50, fg = 'black' ).pack()
+gui_link = Entry(root, width = 50, fg = 'black' )
+
+def user_click():
+    url_string = gui_link.get()
+    # print(gui_link.get())
+    messagebox.showinfo("Finishing Touches", "Thanks for waiting, the program is complete")
+    get_ytube(url_string)
+    
+mybutton = Button(root, text = "Begin Download", command = user_click, padx = 15, pady = 10).pack()
+
 
 #Button and Entry Field
 
-def user_click():
-    messagebox.showinfo("Finishing Touches", "Thanks for waiting, the program is complete")
 
-mybutton = Button(root, text = "Begin Download", command = user_click, padx = 15, pady = 10).pack()
+gui_link.pack()
 
 root.mainloop()
-
-#Write some code in here I tell you what
-
-print("Is this working")
-
-# def get_ytube():
-#     from pytube import YouTube
-#     link = input("enter the link: ")
-#     yt = YouTube(link)
-#     #Get Title
-#     print(yt.title)
-#     #Get Thumbnail Image
-#     print(yt.thumbnail_url)
-#     #select stream resolution
-#     yt = yt.streams.get_highest_resolution()
-#     #or yt = yt.streams.first()
-#     # for video in yt.streams:
-#     #     print(video)
-
-#     yt.download()
-
-
-# # get_ytube()   
